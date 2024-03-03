7be5ac25a377721db54e83ff96f89ae086b02b0f
lien0219 <lien0001@163.com>
2024-03-03 17:51:09 +0800

    路由自动加载

--- src/app/index.js
+++ src/app/index.js
@@ -3,12 +3,18 @@ const KoaBody = require("koa-body");
 
 const errHandler = require("./errHandler");
 
-const userRouter = require("../router/user.route");
+// const userRouter = require("../router/user.route");
+
+// const goodsRouter = require("../router/goods.route");
+const router = require("../router");
 
 const app = new Koa();
 
 app.use(KoaBody());
-app.use(userRouter.routes());
+// app.use(userRouter.routes());
+// app.use(goodsRouter.routes());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.on("error", errHandler);
 
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+// 路由自动加载
+const fs = require("fs");
+
+const Router = require("koa-router");
+const router = new Router();
+
+fs.readdirSync(__dirname).forEach((file) => {
+  // console.log(file)
+  if (file !== "index.js") {
+    let r = require("./" + file);
+    router.use(r.routes());
+  }
+});
+
+module.exports = router;
